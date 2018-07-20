702e403511efc246caaa65b2f152365f40a5cdd9
mtoebes <margaret.toebes@gmail.com>
2018-07-19 20:17:21 -0400

    set fearless filenames

--- user_settings.py
+++ user_settings.py
@@ -1,13 +1,13 @@
 ### User specific values that must be set ###
 
-GUILD_NAME = 'guild name'
+GUILD_NAME = 'Fearless'
 USER_ACCOUNT_NAME = 'my_account'
 GUILD_BANK_ACCOUNT_NAME = 'bank_account'
 WORLD_OF_WARCRAFT_DIR = "E:\Games\World of Warcraft 1.12"
 CREDS_FILE_NAME = 'creds.json'
-TRACKER_SAVED_VARIABLED_FILE_NAME = 'MTGuildTracker.lua'
+TRACKER_SAVED_VARIABLES_FILE_NAME = 'Fearless.lua'
 ENABLE_ICONS = False
 
 BANK_SHEET_INDEX = 0
-LOOT_SHEET_INDEX = 2
-ATTENDANCE_SHEET_INDEX = 3
+LOOT_SHEET_INDEX = 1
+ATTENDANCE_SHEET_INDEX = 2
