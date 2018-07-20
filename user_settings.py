5145c13b7f748bc1d7269c416a8a2f99b46a3ba2
mtoebes <margaret.toebes@gmail.com>
2018-07-19 22:11:38 -0400

    update readme

--- user_settings.py
+++ user_settings.py
@@ -15,7 +15,7 @@ TRACKER_SAVED_VARIABLES_FILE_NAME = 'MTGuildTracker.lua'
 
 ### LegacyPlayer Settings ###
 GUILD_NAME = 'my_guild_name' #TODO UPDATE PRIOR TO USE
-GUILD_REALM = 'Northdale'
+GUILD_REALM = 'Northdale'  #TODO UPDATE PRIOR TO USE
 
 ### Google Sheet Settings ###
 
